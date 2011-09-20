c8726e7f01d5b1d8e9b290c10a0a925fe73d3e4c
Perry Zheng <perry.zheng@gmail.com>
2011-09-20 06:03:13 -0700

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -114,7 +114,11 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to international community/i
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
-    
   end
 end

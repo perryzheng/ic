f710eae84745b8e79b4d36f9c6637549ceb8c3eb
Perry Zheng <perry.zheng@gmail.com>
2011-09-21 03:17:37 -0700

    change to add_column

--- db/migrate/20110921083359_add_many_info_fields_to_user.rb
+++ db/migrate/20110921083359_add_many_info_fields_to_user.rb
@@ -1,16 +1,16 @@
 class AddManyInfoFieldsToUser < ActiveRecord::Migration
   def self.up
-    change_column :users, :first_name,   :string
-    change_column :users, :last_name,    :string
-    change_column :users, :phone,        :string
-    change_column :users, :esl,          :string
-    change_column :users, :country,      :string
-    change_column :users, :start_date,   :string
-    change_column :users, :end_date,     :string
-    change_column :users, :english_help, :string
-    change_column :users, :living_help,  :string
-    change_column :users, :fun_help,     :string    
-    change_column :users, :notes,        :string
+    add_column :users, :first_name,   :string
+    add_column :users, :last_name,    :string
+    add_column :users, :phone,        :string
+    add_column :users, :esl,          :string
+    add_column :users, :country,      :string
+    add_column :users, :start_date,   :string
+    add_column :users, :end_date,     :string
+    add_column :users, :english_help, :string
+    add_column :users, :living_help,  :string
+    add_column :users, :fun_help,     :string    
+    add_column :users, :notes,        :string
   end
 
   def self.down

34c7e85449faf23420c2d177d4da40df63458466
Perry Zheng <perry.zheng@gmail.com>
2011-11-05 23:22:10 -0700

    finished creating microposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   def home
-    if (signed_in?)
-      redirect_to user_path(current_user)
-    else
-      @title = "Home"
+    # if (signed_in?)
+    #   #redirect_to user_path(current_user)
+    #   redirect_to root_path
+    # else
+    #   @title = "Home"
+    # end
+    @title = "Home"
+    if signed_in?
+          @micropost = Micropost.new
+          @feed_items = current_user.feed.paginate(:page => params[:page])
     end
   end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -19,12 +19,12 @@ describe PagesController do
                   :content => "#{@base_title} | Home")
     end
     
-    it "should redirect to show page if the user is signed in" do
-      @user = Factory(:user)
-      test_sign_in(@user)
-      get 'home'
-      response.should redirect_to(user_path(@user))
-    end
+    # it "should redirect to show page if the user is signed in" do
+    #   @user = Factory(:user)
+    #   test_sign_in(@user)
+    #   get 'home'
+    #   response.should redirect_to(user_path(@user))
+    # end
   end
 
   describe "GET 'contact'" do
